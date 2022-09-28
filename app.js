7de8dcf3a192f7ae3992c7f75d8a7692ae45fc19
petermatyas <mtysptr@gmail.com>
2022-09-28 10:20:34 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const mysql = require('mysql2')
 const dotenv = require('dotenv');
 dotenv.config();
 
-const bodyParser=require("body-parser");
+const bodyParser = require("body-parser");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
@@ -16,36 +16,19 @@ let validSum = parseInt(process.env.VALID)
 
 console.log('NODE_ENV:', process.env.NODE_ENV)
 
-/*con = mysql.createConnection({
-    host: process.env.HOST,
+
+const con = mysql.createPool({    // WEB!!!!!!!!!!!!!!!
     user: process.env.USER,
     password: process.env.PASSWORD,
-    //password: '+M}3OEDa5(&9GG^(',
     database: process.env.DATABASE,
-    //socketPath: 'szhely2030:europe-central2:szhely2030'
-})*/
-
-/*const con = mysql.createPool({    // WEBES!!!!!!!!!!!!!!!
-    user: process.env.USER,
-    password: '+M}3OEDa5(&9GG^(',
-    database: process.env.DATABASE,
-    socketPath: '/cloudsql/szhely2030:europe-central2:szhely2030'
-})*/
-
-
-
-const con = mysql.createPool({    // WEBES!!!!!!!!!!!!!!!
-        user: process.env.USER,
-        password: process.env.PASSWORD,
-        database: process.env.DATABASE,
-        socketPath: process.env.SOCKET_PATH,
-    })
+    socketPath: process.env.SOCKET_PATH,
+})
 
 
 
 if (process.env.NODE_ENV == 'developement') {
     con = mysql.createConnection({
-        host: '192.168.1.82',
+        host: process.env.HOST,
         user: process.env.USER,
         password: process.env.PASSWORD,
         database: process.env.DATABASE,
@@ -56,9 +39,7 @@ if (process.env.NODE_ENV == 'developement') {
         if (err) throw err;
         //console.log("Connected!");
     });
-
-
-
+}
 
 
 function isNumeric(value) {
@@ -86,19 +67,19 @@ function isVoteTimePromise() {
         let sql = "SELECT * FROM votestart";
         con.query(sql, (err, queryRes) => {
             //if (err) throw (err);
-            if (err) {return reject(err)}
+            if (err) { return reject(err) }
             resolve(queryRes[0].ontime)
         })
     })
 }
 
 function isIdNotExistPromise(new_id_qr) {
-    return new Promise(function (resolve,reject) {
+    return new Promise(function (resolve, reject) {
         let sql = "SELECT id_qr FROM votes";
         con.query(sql, (err, queryRes) => {
             //if (err) throw (err);
-            if (err) {return reject(err)}
-            for (var i=0; i<queryRes.length; i++) {
+            if (err) { return reject(err) }
+            for (var i = 0; i < queryRes.length; i++) {
                 if (queryRes[i].id_qr == new_id_qr) {
                     resolve(false)
                 }
@@ -106,12 +87,12 @@ function isIdNotExistPromise(new_id_qr) {
             resolve(true)
         })
     });
-  }
+}
 
 function insertVoteToDbPromise(id_qr, selected) {
     return new Promise(function (resolve, reject) {
         const time = new Date()
-        post = {id_qr:id_qr, time:time, vote_to:selected}
+        post = { id_qr: id_qr, time: time, vote_to: selected }
         let sql = "INSERT INTO votes SET ?";
         con.query(sql, post, (err, result) => {
             if (err) throw (err);
@@ -122,7 +103,7 @@ function insertVoteToDbPromise(id_qr, selected) {
 
 function updateStartStop(updateTo) {
     return new Promise(function (resolve, reject) {
-        post = {id:0, ontime:updateTo}
+        post = { id: 0, ontime: updateTo }
         let sql = "UPDATE votestart SET ?";
         con.query(sql, post, (err, result) => {
             if (err) throw (err);
@@ -156,16 +137,16 @@ app.get('/', (req, res) => {
                 if (result == 1) {
                     return isIdNotExistPromise(req.query.id).then((result) => {
                         if (result) {
-                            res.render('index', {qr_id:req.query.id});
+                            res.render('index', { qr_id: req.query.id });
                         } else {
                             res.sendFile(path.resolve(__dirname, './views/alreadyvoted.html'));
-                        } 
+                        }
                     })
                 } else {
                     res.sendFile(path.resolve(__dirname, './views/novotingtime.html'));
                 }
             })
-            
+
         } else {
             return res.sendFile(path.resolve(__dirname, './views/wrongid.html'))
         }
@@ -180,7 +161,7 @@ app.post('/', (req, res) => {
     console.log('selected', selected, '  id_qr', id_qr)
 
     isVoteTimePromise().then((result) => {
-        
+
         if (result == 1) {
             //console.log('-->', selected, id_qr)
             if (typeof selected !== 'undefined') {
@@ -201,7 +182,7 @@ app.post('/', (req, res) => {
             res.sendFile(path.resolve(__dirname, './views/novotingtime.html'));
         }
     })
-    
+
 
 })
 
@@ -210,16 +191,16 @@ app.post('/', (req, res) => {
 app.get('/admin', (req, res) => {
     startstopBtnText = 'valamiGet'
     isVoteTimePromise().then((result) => {
-        if (result == 0) {  
+        if (result == 0) {
             startstopBtnText = 'Kezdődjön'
             //res.render('admin', {startstopBtn:'Kezdődjön', ip:ip});
         } else {
             startstopBtnText = 'Vége'
             //res.render('admin', {startstopBtn:'Vége', ip:ip});
         }
-        res.render('admin', {startstopBtn:startstopBtnText});
+        res.render('admin', { startstopBtn: startstopBtnText });
     })
-    
+
     //res.render('admin', {startstopBtn:'Kezdődjön', ip:ip});
 
 
@@ -241,14 +222,14 @@ app.post('/admin', (req, res) => {
     isVoteTimePromise().then((result) => {
         //('isVoteTimePromise result', result)
         if (result == 0) {
-            updateStartStop(1).then((result) => {})
-            res.render('admin', {startstopBtn:'Vége'});
+            updateStartStop(1).then((result) => { })
+            res.render('admin', { startstopBtn: 'Vége' });
         } else {
-            updateStartStop(0).then((result) => {})
-            res.render('admin', {startstopBtn:'Kezdődjön'});
+            updateStartStop(0).then((result) => { })
+            res.render('admin', { startstopBtn: 'Kezdődjön' });
         }
     })
-    
+
 
 })
 
@@ -258,7 +239,7 @@ app.get('/api/participants', (req, res) => {
     let sql = "SELECT id, name FROM participants WHERE participants.visible > 0";
     con.query(sql, (err, queryRes) => {
         if (err) throw (err);
-        res.status(200).json({data:queryRes})
+        res.status(200).json({ data: queryRes })
     });
 
 })
@@ -275,7 +256,7 @@ app.get('/api/summary', (req, res) => {
     con.query(sql, (err, queryRes) => {
         if (err) throw (err);
         //console.log(queryRes)
-        queryRes.sort(function(a, b){
+        queryRes.sort(function (a, b) {
             return b.votes - a.votes;
         });
 
@@ -292,12 +273,9 @@ app.get('/api/votetime', (req, res) => {
 
 })
 
-if (process.env.NODE_ENV == 'developement') {
-    app.listen(5000, () => {
-        console.log('server lisening on port 5000')
-    })
-} else if (process.env.NODE_ENV == 'production') {
-    app.listen(8080, () => {
-        console.log('server lisening on port 8080')
-    })
-}
+
+app.listen(parseInt(process.env.PORT), () => {
+    console.log('server lisening on port ', process.env.PORT)
+})
+
+
